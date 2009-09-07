0e1bb703d586025fd487f116128c2f1250a69e36
bobpp <bobpp.asroma+github@gmail.com>
2009-09-08 01:49:13 +0900

    MobaSiF::Template First commit
    - Rename MTemplate to MobaSiF::Template
    - Change C++ style comment to C style comment on Template.xs

---
+++ Template.pm
@@ -0,0 +1,42 @@
+package MobaSiF::Template;
+
+use 5.008;
+use strict;
+use warnings;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+our %EXPORT_TAGS = ( 'all' => [ qw() ] );
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+our @EXPORT = qw();
+our $VERSION = '0.01';
+
+require XSLoader;
+XSLoader::load('MobaSiF::Template', $VERSION);
+
+1;
+
+__END__
+
+=head1 NAME
+
+MobaSiF::Template - 高速なテンプレートライブラリ
+
+=head1 SYNOPSIS
+
+use MobaSiF::Template;
+$html = MobaSiF::Template::insert($compiled_template_file, $refParamHash);
+
+=head1 ABSTRACT
+
+事前コンパイルされたテンプレートバイナリを用いて高速なテンプレート処理を行います。ループ・条件分岐・置換（URL ENCODE, HTMLSPECIALCHARS(+NL2BR) が可能）に対応しており、基本的なHTMLテンプレートの処理に対応できます。
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+MobaSiF::Template::Compiler
+
+=cut
