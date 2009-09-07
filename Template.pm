c92e491424d84bad2aade8035c64443e752fd64a
bobpp <bobpp.asroma+github@gmail.com>
2009-09-08 02:46:56 +0900

    Auto Compile enable.
    - $MobaSiF::Template::DEVELOP = 1
    - Auto Compile when template file is newer than compiled template file.

--- Template.pm
+++ Template.pm
@@ -4,10 +4,12 @@ use 5.008;
 use strict;
 use warnings;
 
+use File::stat;
+use MobaSiF::Template::Compiler;
+
 require Exporter;
 
 our @ISA = qw(Exporter);
-
 our %EXPORT_TAGS = ( 'all' => [ qw() ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw();
@@ -15,6 +17,20 @@ our $VERSION = '0.01';
 
 require XSLoader;
 XSLoader::load('MobaSiF::Template', $VERSION);
+
+our $DEVELOP = 0;
+sub render {
+	my ($template, $compiled_template, $param, $param2, $param3) = @_;
+	if ($DEVELOP) {
+		my $template_stat = stat($template);
+		my $compiled_stat = stat($compiled_template);
+
+		if ($template_stat->mtime > $compiled_stat->mtime) {
+			MobaSiF::Template::Compiler::compile($template, $compiled_template);
+		}
+	}
+	return MobaSiF::Template::insert($compiled_template, $param, $param2, $param3);
+}
 
 1;
 
